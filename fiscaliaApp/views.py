9ea6e57bab5aa2b83def49bfe6dc3e5f38f5810c
PaulAlexander19 <pluquec@unsa.edu.pe>
2022-07-21 19:40:24 -0500

    hito 2

--- fiscaliaApp/views.py
+++ fiscaliaApp/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from httplib2 import Http
 
 # Create your views here.
+## aquil es la logica
+def mainView(request, *args, **kwargs):
+    
+    return render(request, 'fiscaliaApp/MainSearch.html')
+
+def fiscaliaView(request, *args, **kwargs):
+    return render(request, 'fiscaliaApp/FiscaliaPage.html')
+
+def testView(request, *args, **kwargs):
+    ## retorna un h1
+    return HttpResponse('<h1>Hola mundo</h1>') 
