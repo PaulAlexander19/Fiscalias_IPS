a6d3d144a86267c8f55e4122b7c9fa6fcef88b5e
Andy Briyit <ahuamanita@unsa.edu.pe>
2022-07-23 01:38:19 -0500

    se corrigio errores de modelo

--- Fiscalias/models.py
+++ Fiscalias/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-class Fiscalias:
+from Usuario.models import Usuario
+class Fiscalias(models.Model):
     FisID = models.AutoField(primary_key=True)
     CodSiga = models.CharField(max_length=10, blank=True, null=True)
     Nombre = models.CharField(max_length=255, blank=True, null=True)
@@ -11,12 +11,18 @@ class Fiscalias:
     Provincia = models.CharField(max_length=100, blank=True, null=True)
     Departamento = models.CharField(max_length=100, blank=True, null=True)
     Direccion = models.CharField(max_length=255, blank=True, null=True)
-    Correo = models.CharField(max_length=255, blank=True, null=True)
+    Correo = models.EmailField(max_length=255, blank=True, null=True)
     Telefono = models.CharField(max_length=255, blank=True, null=True)
 
-class Consulta:
+    def __str__(self):
+        return self.Nombre
+
+class Consulta(models.Model):
     ConID = models.AutoField(primary_key=True)
-    UserID = models.ForeignKey('Usuario', models.DO_NOTHING, blank=True, null=True)
-    Fecha = models.DateField(blank=True, null=True)
-    FisID = models.ForeignKey('Fiscalias', models.DO_NOTHING, blank=True, null=True)
+    UserID = models.ForeignKey(Usuario, models.DO_NOTHING, blank=True, null=True) # UserID es un Objeto
+    FisID = models.ForeignKey(Fiscalias, models.DO_NOTHING, blank=True, null=True)
+    Fecha = models.DateField(blank=True, null=True, auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.ConID) + " -> " + self.UserID.first_name + " - " + self.FisID.Nombre        
 
