a01234dbbb80b60c2fbb89b7186f8367b450b124
Julio <jpaucarc@unsa.edu.pe>
2022-07-26 16:31:53 -0500

    Intento de formulario para login de usuario

--- Usuario/urls.py
+++ Usuario/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
   UsuarioRegister,
+  UsuarioLogin,
 )
 
 urlpatterns = [
-    path("registro", UsuarioRegister, name="usuario-register"),
+    path("registro/", UsuarioRegister, name="usuario-register"),
+    path("login/", UsuarioLogin, name="usuario-login"),
 ]
--- Usuario/views.py
+++ Usuario/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .models import Usuario
 from .forms import UsuarioForm
 
@@ -7,9 +8,19 @@ def UsuarioRegister(request):
   form = UsuarioForm(request.POST or None)
   if (form.is_valid()):
     form.save()
-    form = UsuarioForm()
+    return redirect('../')
+
 
   context = {
     'form': form 
   }
   return render(request, 'Usuario/usuarioRegister.html', context)
+
+def UsuarioLogin(request):
+  username = request.POST['username']
+  password = request.POST['password']
+  user = authenticate(request, username=username, password=password)
+  if user is not None:
+    login(request, user)
+  else:
+    pass
