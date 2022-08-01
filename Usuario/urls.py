13ec3e2abef69100f076ceb6fc9e638534e4c68d
RicardoSilvaT <rsilvato@unsa.edu.pe>
2022-07-31 20:49:17 -0500

    se modifico urls y views de Usuario

--- Usuario/urls.py
+++ Usuario/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import (
   UsuarioRegister,
   UsuarioLogin,
 )
 
 urlpatterns = [
-    path("registro/", UsuarioRegister, name="usuario-register"),
-    path("login/", UsuarioLogin, name="usuario-login"),
+    path("perfil/", views.UsuarioPerfil, name="usuario-perfil"),
+    path("registro/", views.UsuarioRegister, name="usuario-register"),
+    path("login/", LoginView.as_view(template_name='Usuario/login.html'), name="usuario-login"),
+    path("logout/", LogoutView.as_view(template_name='Usuario/logout.html'), name="usuario-logout"),
 ]
--- Usuario/views.py
+++ Usuario/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .models import Usuario
+from .models import *
 from .forms import UsuarioForm
+from django.contrib import messages
 
 # Create your views here.
 def UsuarioRegister(request):
-  form = UsuarioForm(request.POST or None)
-  if (form.is_valid()):
-    form.save()
-    return redirect('../')
+  if request.method == 'POST':
+    form = UsuarioForm(request.POST or None)
+    if (form.is_valid()):
+      form.save()
+      #return redirect('/fiscalias/')
+      username = form.cleaned_data['username']
+      messages.success(request, f'Usuario {username} creado')
+  else:
+    form = UsuarioForm() 
 
-
-  context = {
-    'form': form 
-  }
+  context = {'form': form }
   return render(request, 'Usuario/usuarioRegister.html', context)
 
+
 def UsuarioLogin(request):
-  username = request.POST['username']
-  password = request.POST['password']
+  username = request.POST.get('username')
+  password = request.POST.get('password')
   user = authenticate(request, username=username, password=password)
   if user is not None:
     login(request, user)
   else:
     pass
+
+  
+
+def UsuarioPerfil(request):
+  return render(request, 'Usuario/usuarioRegister.html', {'user':user, 'posts':posts})
