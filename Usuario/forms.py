234d2f1a9642183cd4f68ea41d24a4a32be9f463
RicardoSilvaT <rsilvato@unsa.edu.pe>
2022-07-31 20:46:02 -0500

    se modifico el forms de usuario

--- Usuario/forms.py
+++ Usuario/forms.py
@@ -1,16 +1,23 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import Usuario
 
-class UsuarioForm(forms.ModelForm):
+class UsuarioForm(UserCreationForm):
+    email = forms.EmailField()
+    password1 = forms.CharField(label='Constraseña', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirma Constraseña', widget=forms.PasswordInput)
     
     class Meta:
         model = Usuario
         fields = [
-          "first_name",
-          "last_name",
           "username",
-          "password",
-          "Direccion",
-          "Departamento",
-          "Provincia",
+          "email",
+          "password1",
+          "password2",
+          #"first_name",
+          #"last_name",
+          #"Direccion",
+          #"Departamento",
+          #"Provincia",
         ]
